230efbcae2dbaa5184601d5643488fac4fc948d6
MarcioFujita <hanker78@gmail.com>
2021-10-25 18:25:52 -0300

    endpoint

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require('express')();
 const cors = require('cors');
 const server = require('http').createServer(app);
+const { getAll, preStart } = require('./Models/itemModel');
 
 const CORS_OPTIONS = {
   origin: 'http://localhost:3000',
@@ -12,5 +13,16 @@ const io = require('socket.io')(server, {cors: CORS_OPTIONS});
 app.use(cors(CORS_OPTIONS));
 
 const PORT = 3001;
+
+app.get('/items', async (_req, res) => {
+  try {
+    await preStart();
+    const items = await getAll();
+    return res.status(200).json(items);
+  } catch (error) {
+    res.status(500).end();
+    console.log(error.message);
+  }
+})
 
 server.listen(PORT, () => console.log(`Estou na porta ${PORT}`));
